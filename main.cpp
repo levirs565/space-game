a84b1231ef1c679f159f5124843e96f0f716bc00
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-05-07 04:51:42 +0700

    feat: simple sap

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <exception>
 #include <queue>
 #include <ranges>
+#include <map>
 
 class TextureLoader {
 public:
@@ -233,6 +234,7 @@ public:
     bool mustGone = false;
     std::vector<Vec2> boundingBox;
     double boundingRadius = 0;
+    double x0, y0, x1, y1;
 
     GameEntity(const Vec2 &position, double angle) : position(position), angle(angle) {}
 
@@ -292,6 +294,391 @@ public:
         boundingBox.push_back(bottomRight);
         boundingBox.push_back(bottomLeft);
         boundingBox.push_back(topLeft);
+
+        auto [minX, maxX] = std::minmax_element(boundingBox.begin(), boundingBox.end(), [] (const Vec2& a, const Vec2& b) { return a.x < b.x; });
+        auto [minY, maxY] = std::minmax_element(boundingBox.begin(), boundingBox.end(), [] (const Vec2& a, const Vec2& b) { return a.y < b.y; });
+        x0 = minX->x;
+        x1 = maxX->x;
+        y0 = minY->y;
+        y1 = maxY->y;
+    }
+};
+
+class SAPDimension {
+public:
+    using CollisionMap = std::unordered_map<GameEntity*, std::set<GameEntity*>>;
+
+    struct Item {
+        double value;
+        int interval;
+        GameEntity* entity;
+        int stabs = 0;
+        bool isPendingDelete = false;
+
+        Item(double value, int interval, GameEntity* entity): value{value}, interval{interval}, entity{entity} {
+
+        }
+
+        bool operator < (const Item& other) {
+            return value < other.value || (value == other.value && (
+                ( entity == other.entity && interval < other.interval ) ||
+                (interval > other.interval) 
+            ));
+        }
+    };
+
+    bool isY;
+    std::vector<std::shared_ptr<Item>> intervalList;
+    std::vector<std::shared_ptr<Item>> bufferList;
+    CollisionMap* mCollisionMap;
+
+    void swapCallback(std::shared_ptr<Item>& item2, std::shared_ptr<Item>& item1) {
+        GameEntity* entity1 = item1->entity;
+        GameEntity* entity2 = item2->entity;
+
+        if (item1->interval == 0 && item2->interval == 1) {
+            setPair(entity1, entity2);
+
+            item2->stabs++;
+            item1->stabs++;
+        } else if (item1->interval == 1 && item2->interval == 0) {
+            (*mCollisionMap)[entity1].erase(entity2);
+            (*mCollisionMap)[entity2].erase(entity1);
+
+            item2->stabs--;
+            item1->stabs--;
+        } else {
+            std::swap(item1->stabs, item2->stabs);
+        }
+    }
+
+    void insertSort(size_t i) {
+        std::shared_ptr<Item> v = std::move(intervalList[i]);
+        int k = i - 1;
+        while (k >= 0 && *(v.get()) < *(intervalList[k].get())) {
+            swapCallback(intervalList[k], v);
+            intervalList[k+1] = std::move(intervalList[k]);
+            k--;
+        }
+        intervalList[k+1] = std::move(v);
+    }
+
+    void setStabs(size_t i) {
+        if (i > 0) {
+            int leftStab = intervalList[i - 1]->stabs;
+            if (intervalList[i - 1]->interval == 1) {
+                intervalList[i]->stabs = leftStab - 1;
+            } else {
+                intervalList[i]->stabs = leftStab + 1;
+            }
+        } else {
+            intervalList[i]->stabs = 0;
+        }
+    }
+
+    void setPair(GameEntity* entity1, GameEntity* entity2) {
+        if (
+            entity1->x0 < entity2->x1 &&
+            entity1->x1 > entity2->x0 &&
+            entity1->y0 < entity2->y1 &&
+            entity1->y1 > entity2->y0
+        ) {
+            (*mCollisionMap)[entity1].insert(entity2);
+            (*mCollisionMap)[entity2].insert(entity1);
+        }
+    }
+
+    void processSets(std::shared_ptr<Item>& item, std::set<GameEntity*>& setMaintain, std::initializer_list<std::set<GameEntity*>*> setCollide) {
+        if (item->interval == 0) {
+            setMaintain.insert(item->entity);
+        } else {
+            setMaintain.erase(item->entity);
+            for (auto set : setCollide) {
+                for (GameEntity* other : *set) {
+                    setPair(item->entity, other);
+                }
+            }
+        }
+    }
+
+    void run() {
+        std::set<GameEntity*> setInsert, setInterval;
+
+        bool checkStab = false;
+
+        size_t i = 0;
+        size_t j = 0; 
+
+        std::sort(
+            bufferList.begin(), 
+            bufferList.end(), 
+            [] (const std::shared_ptr<Item>& a,const std::shared_ptr<Item>& b) {
+            return *(a.get()) < *(b.get());
+        });
+
+        while (i < intervalList.size()) {
+            std::shared_ptr<Item>& item = intervalList[i];
+
+            if (item->isPendingDelete) {
+                intervalList.erase(intervalList.begin() + i);
+                checkStab = true;
+                continue;
+            } 
+
+            bool hasNew = j < bufferList.size();
+            if (hasNew) {
+                std::shared_ptr<Item>& newItem = bufferList[j];
+
+                if (newItem->isPendingDelete) {
+                    checkStab = true;
+                    j++;
+                    continue;
+                }
+
+                if (newItem < item) {
+                    if (isY) {
+                        processSets(newItem, setInsert, {&setInsert, &setInterval});
+                    }
+
+                    intervalList.insert(intervalList.begin() + i, newItem);
+                    setStabs(i);
+                    i++;
+                    j++;
+
+                    checkStab = true;
+
+                    continue;
+                }
+            }
+
+            if (checkStab) setStabs(i);
+
+            if (j < bufferList.size() && isY) {
+                processSets(item, setInterval, {&setInsert});
+            }
+
+            insertSort(i);
+            i++;
+        }
+
+        while (j < bufferList.size()) {
+            std::shared_ptr<Item>& newItem = bufferList[j];
+
+            if (!newItem->isPendingDelete) {
+                if (isY) {
+                    processSets(newItem, setInsert, {&setInsert});
+                }
+
+                intervalList.insert(intervalList.begin() + i, newItem);
+                setStabs(i);
+                i++;
+            }
+
+            j++;
+        }
+        bufferList.clear();
+
+        if (intervalList.size() < i) {
+            intervalList.erase(intervalList.begin() + i, intervalList.end());
+        }
+
+        bufferList.clear();
+
+        double prev = std::numeric_limits<double>::lowest();
+        for (const std::shared_ptr<Item>& item : intervalList) {            
+            if (item->value < prev) throw std::domain_error("Sorting faield");
+            prev = item->value;
+        }
+    }
+
+    std::vector<size_t> iterateStabs(size_t index) {
+        if (index <= intervalList.size()) return {};
+
+        std::set<GameEntity*> skip;
+        int stabs = intervalList[index]->stabs;
+        std::vector<size_t> result;
+
+        size_t i = index;
+        while (stabs > 0 && i > 0) {
+            i--; 
+            std::shared_ptr<Item>& item = intervalList[i];
+            if (item->interval == 0 && !skip.contains(item->entity)) {
+                stabs--;
+                result.push_back(i);
+                continue;
+            }
+            skip.insert(item->entity);
+        }
+
+        return result;
+    }
+
+    void addStabsToSet(size_t index, std::unordered_map<GameEntity*, int>& set) {
+        for (size_t index : iterateStabs(index)) {
+            GameEntity* entity = intervalList[index]->entity;
+            set[entity] = set.contains(entity) ? set[entity] + 1 : 1;
+        }
+    }
+
+    std::pair<int, int> binarySearch(double value) {
+        int start = 0, end = intervalList.size() - 1;
+        
+        int lower = -1, upper = intervalList.size();
+
+        while (start <= end) {
+            size_t mid = (start + end) / 2;
+            double currentValue = intervalList[mid]->value;
+
+            if (value == currentValue) {
+                lower = mid;
+                upper = mid;
+
+                size_t num = mid - 1;
+                while (num > 0 && value == intervalList[num]->value) {
+                    lower = num;
+                    num--;
+                }
+
+                num = mid + 1;
+
+                while (num < intervalList.size() && value == intervalList[num]->value) {
+                    upper = num;
+                    num++;
+                }
+
+                return {lower, upper};
+            } else if (value < currentValue) {
+                upper = mid;
+                end = mid - 1;
+            } else {
+                lower = mid;
+                start = mid + 1;
+            }
+        }
+
+        return {lower, upper};
+    }
+
+public:
+
+    SAPDimension(bool isY, CollisionMap* collisionMap): isY{isY}, mCollisionMap{collisionMap} {
+
+    }
+};
+
+class SAP {
+    struct Item {
+        std::shared_ptr<SAPDimension::Item> x0, y0, x1, y1;
+    };
+
+    std::unordered_map<GameEntity*, Item> entities;
+    SAPDimension::CollisionMap mCollisionList;
+    std::vector<GameEntity*> mPendingRemove;
+
+    SAPDimension dimensionX{false, &mCollisionList};
+    SAPDimension dimensionY{true, &mCollisionList};
+
+public:
+    void add(GameEntity* entity) {
+        Item item{
+            .x0 = std::make_shared<SAPDimension::Item>(
+                entity->x0,
+                0,
+                entity
+            ),
+            .y0 = std::make_shared<SAPDimension::Item>(
+                entity->y0,
+                0,
+                entity
+            ),
+            .x1 = std::make_shared<SAPDimension::Item>(
+                entity->x1,
+                1,
+                entity
+            ),
+            .y1 = std::make_shared<SAPDimension::Item>(
+                entity->y1,
+                1,
+                entity
+            )
+        };
+
+        dimensionX.bufferList.push_back(item.x0);
+        dimensionX.bufferList.push_back(item.x1);
+        dimensionY.bufferList.push_back(item.y0);
+        dimensionY.bufferList.push_back(item.y1);
+
+        entities[entity] = std::move(item);
+    }
+
+    void move(GameEntity* entity) {
+        if (!entities.contains(entity)) return;
+
+        Item& item = entities[entity];
+        item.x0->value = entity->x0;
+        item.x1->value = entity->x1;
+        item.y0->value = entity->y0;
+        item.y1->value = entity->y1;
+    }
+
+    void remove(GameEntity* entity) {
+        if (!entities.contains(entity)) return;
+
+        Item& item = entities[entity];
+        item.x0->isPendingDelete = true;
+        item.x1->isPendingDelete = true;
+        item.y0->isPendingDelete = true;
+        item.y1->isPendingDelete = true;
+        mPendingRemove.push_back(entity);
+    }
+
+    void update() {
+        dimensionX.run();
+        dimensionY.run();
+
+        for (GameEntity* entity : mPendingRemove) {
+            for (auto& [_, collisionList] : mCollisionList) {
+                collisionList.erase(entity);
+            }
+
+            mCollisionList.erase(entity);
+            entities.erase(entity);
+        }
+
+        mPendingRemove.clear();
+    }
+
+    std::vector<GameEntity*> queryArea(double x0, double y0, double x1, double y1, bool enclosed) {
+        int minScore = enclosed ? 3 : 0;
+        auto [_xl, xi] = dimensionX.binarySearch(x0);
+        auto [_yl, yi] = dimensionY.binarySearch(y0);
+        std::unordered_map<GameEntity*, int> xSet, ySet;
+
+        if (!enclosed) {
+            dimensionX.addStabsToSet(xi, xSet);
+            dimensionX.addStabsToSet(yi, ySet);
+        }
+
+        while (xi < dimensionX.intervalList.size() && dimensionX.intervalList[xi]->value <= x1) {
+            GameEntity* entity = dimensionX.intervalList[xi]->entity;
+            xSet[entity] = xSet.contains(entity) ? xSet[entity] + 2 : 2;
+            xi++;
+        }
+
+        while (yi < dimensionY.intervalList.size() && dimensionY.intervalList[yi]->value <= y1) {
+            GameEntity* entity = dimensionY.intervalList[yi]->entity;
+            ySet[entity] = ySet.contains(entity) ? ySet[entity] + 2 : 2;
+            yi++;
+        }
+
+        std::vector<GameEntity*> result;
+        for (auto [entity, xScore] : xSet) {
+            if (ySet.contains(entity) && xScore > minScore && ySet[entity] > minScore) {
+                result.push_back(entity);
+            }
+        }
+
+        return result;
     }
 };
 
@@ -1622,16 +2009,16 @@ public:
 
         std::unique_ptr<PlayerShip> playerShip = std::make_unique<PlayerShip>(mTextureLoader.get(), Vec2(400, 700));
         mPlayerShip = playerShip.get();
-        mEntityList.push_back(std::move(playerShip));
+        addEntity(std::move(playerShip));
 
-        mEntityList.push_back(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(100, 0))));
-        mEntityList.push_back(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(300, 0))));
-        mEntityList.push_back(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(600, 0))));
+        addEntity(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(100, 0))));
+        addEntity(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(300, 0))));
+        addEntity(std::move(std::make_unique<Enemy>(mTextureLoader.get(), Vec2(600, 0))));
 
-        mEntityList.push_back(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(100, 500), "Brown_big1")));
-        mEntityList.push_back(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(300, 500), "Brown_big2")));
-        mEntityList.push_back(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(600, 500), "Brown_big3")));
-        mEntityList.push_back(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(615, 1000), "Brown_big3")));
+        addEntity(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(100, 500), "Brown_big1")));
+        addEntity(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(300, 500), "Brown_big2")));
+        addEntity(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(600, 500), "Brown_big3")));
+        addEntity(std::move(std::make_unique<Meteor>(mTextureLoader.get(), Vec2(615, 1000), "Brown_big3")));
         mPathFinder.init(mWordSize, 110);
 
         mPathFinder.clearState();
@@ -1643,6 +2030,12 @@ public:
         }
 
         mPathFinder.generateHeatmap(mPlayerShip->position);
+    }
+
+    void addEntity(std::unique_ptr<GameEntity>&& entity) {
+        GameEntity* ptr = entity.get();
+        mEntityList.push_back(std::move(entity));
+        mSAP.add(ptr);
     }
 
     void processKeyDown(const SDL_KeyboardEvent &key) {
@@ -1756,11 +2149,14 @@ public:
 
             size_t entityCount = mEntityList.size();
             for (size_t i = 0; i < entityCount; i++) {
-                const std::unique_ptr<GameEntity> &entity = mEntityList[i];
+                GameEntity* entity = mEntityList[i].get();
                 entity->onTick(this);
+                mSAP.move(entity);
                 // setelah onTick, jangan gunakan entity kembali karena ada kemungkinan penambahan elemen ke mEntityList
                 // yang menyebabkan operasi std::move terhadap entity sehingga entity berada dalam keadaan invalid
             }
+
+            mSAP.update();
 
             calculateCamera();
             drawBackground();
@@ -1792,7 +2188,9 @@ public:
                     it = mEntityList.erase(it) - 1;
                     continue;
                 }
+            }
 
+            for (GameEntity* entity : mSAP.queryArea(mCameraPosition.x, mCameraPosition.y, mCameraPosition.x + mCameraSize.x, mCameraPosition.y + mCameraSize.y, false)) {
                 entity->onDraw(mRenderer, mCameraPosition);
 
                 for (size_t i = 0; i < entity->boundingBox.size(); i++) {
@@ -1820,7 +2218,7 @@ public:
     void addLaser(const Vec2 &position, double angle) override {
         std::unique_ptr<Laser> laser = std::make_unique<Laser>(mTextureLoader.get(), position,
                                                                angle);
-        mEntityList.push_back(std::move(laser));
+        addEntity(std::move(laser));
         Mix_PlayChannel(1, mLaserSound, 0);
     }
 
@@ -1861,6 +2259,7 @@ private:
     bool mIsDown = false;
     bool mIsRight = false;
     bool mIsFire = false;
+    SAP mSAP;
 };
 
 int main() {
